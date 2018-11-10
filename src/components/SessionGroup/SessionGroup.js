eb3077df0c997e5de732b784434187239d4d0a88
Eshed Margalit <eshed.margalit@gmail.com>
2018-11-09 18:54:07 -0800

    prettier rendering of speaker names

--- src/components/SessionGroup/SessionGroup.js
+++ src/components/SessionGroup/SessionGroup.js
@@ -1,4 +1,5 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
+import { FaEnvelope, FaGlobeAmericas, FaLinkedin } from 'react-icons/fa';
 import { Container, Row, Col, ListGroup, ListGroupItem, } from 'reactstrap';
 import moment from 'moment';
 
@@ -11,6 +12,72 @@ class SessionGroup extends Component {
         }
     }
 
+    render_speakers = speakers => {
+        const n_speakers = speakers.length;
+
+        return speakers.map((speaker, i) => {
+            // conditional rendering
+            var speaker_info = <span>
+                <strong>{speaker.name}</strong>
+            </span>
+
+            if (speaker.email && !speaker.url) {
+                speaker_info = <span>
+                    <strong>{speaker.name}</strong>&nbsp;
+                    <a href={"mailto:" + speaker.email}>
+                        <FaEnvelope color="#8c1515" />
+                    </a>
+                </span>
+            }
+            if (!speaker.email && speaker.url) {
+                speaker_info = <span>
+                    <strong>{speaker.name}</strong>&nbsp;
+                    <a href={speaker.url}>
+                        <FaGlobeAmericas color="#4d4f53" />
+                    </a>
+                </span>
+            }
+            if (speaker.email && speaker.url) {
+                var icon = <FaGlobeAmericas color="#4d4f53" />
+                if (speaker.url.includes('linkedin')) {
+                    icon = <FaLinkedin color="#4d4f53" />
+                }
+                speaker_info =
+                        <span>
+                            <strong>{speaker.name}</strong>&nbsp;
+                            <a href={"mailto:" + speaker.email}>
+                                <FaEnvelope color="#8c1515" />
+                            </a>&nbsp;
+                            <a href={speaker.url}>
+                                {icon}
+                            </a>
+                        </span>
+            }
+
+            let to_render;
+
+            if (i === n_speakers - 1){ // last
+                to_render = <span>
+                    and {speaker_info}<br/>
+                </span>
+            } else if (i === n_speakers - 2) { // penultimate
+                to_render = <span>
+                    {speaker_info}&nbsp;
+                </span>
+            } else { //all others
+                to_render = <span>
+                    {speaker_info},&nbsp;
+                </span>
+            }
+
+            return (
+                <span key={speaker.name}>
+                    {to_render}
+                </span>
+            );
+        });
+    }
+
     render_sessions = sessions => {
         if (sessions.length === 0) {
             return (
@@ -21,9 +88,10 @@ class SessionGroup extends Component {
             return (
                 <ListGroupItem key={session.date}>
                     <div>
-                        <h5>{session.title}</h5>
+                        <h4>{session.title}</h4>
                         {moment(session.date).format('MM/DD/YYYY')}
-                        <p>Presented by {session.speakers}</p>
+                        <hr />
+                        {this.render_speakers(session.speakers)}
                         {session.location}, {session.time}
                     </div>
                 </ListGroupItem>
