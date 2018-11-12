71b2d286cbf40d3dc4343cb734a88d6b39dd1253
Eshed Margalit <eshed.margalit@gmail.com>
2018-11-12 14:30:03 -0800

    removes old iframe import

--- src/components/Discussions/Discussions.js
+++ src/components/Discussions/Discussions.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
-import Iframe from 'react-iframe';
 
 class Discussions extends Component {
   render() {
