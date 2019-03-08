d73d68ee2e0b119230ffcded10b1f16da5f9a3bf
Eshed Margalit <eshed.margalit@gmail.com>
2019-03-08 08:38:51 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6195d2d960f6f1183cd74636ae32ba84.js"
+  "/precache-manifest.69d16c0f7571ec3bc4b2c54a20d6d28b.js"
 );
 
 workbox.clientsClaim();
