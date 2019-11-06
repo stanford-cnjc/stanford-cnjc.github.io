b0afd7301746220136a9de55ece694366bc5ee95
tgfisher <tuckergfisher@gmail.com>
2019-11-06 09:33:41 -0800

    Updates

---
+++ precache-manifest.473e1ec0c81c3744f3ffb941fbbd48ae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ead2236b1512357a4fb49fe0d0c461e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2946e246e311346a01f9",
+    "url": "/static/css/2.1e66eaca.chunk.css"
+  },
+  {
+    "revision": "1318679abc5b54413cec",
+    "url": "/static/css/main.5b142289.chunk.css"
+  },
+  {
+    "revision": "2946e246e311346a01f9",
+    "url": "/static/js/2.98982645.chunk.js"
+  },
+  {
+    "revision": "1318679abc5b54413cec",
+    "url": "/static/js/main.3e617901.chunk.js"
+  },
+  {
+    "revision": "5854c3709e1904f4eab2",
+    "url": "/static/js/runtime~main.f1fd0f00.js"
+  },
+  {
+    "revision": "05342f4ca8e9ab746803de8a63aad3cb",
+    "url": "/static/media/mbct-logo.05342f4c.png"
+  },
+  {
+    "revision": "496c3ff7aa626a5cea7cbe50ee15fdc9",
+    "url": "/static/media/wt_400x400.496c3ff7.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.65f85daf8b4e80366845654c233f50a0.js"
+  "/precache-manifest.473e1ec0c81c3744f3ffb941fbbd48ae.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
