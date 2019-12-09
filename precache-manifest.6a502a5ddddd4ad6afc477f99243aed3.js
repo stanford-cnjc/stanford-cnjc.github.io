da5ba54f30b348073ff8f4be991b50cbc47155ce
Eshed Margalit <eshed.margalit@gmail.com>
2019-12-08 21:41:08 -0800

    Updates

---
+++ precache-manifest.6a502a5ddddd4ad6afc477f99243aed3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82e497f41035c8b9f8645bf94a606e15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07cc99008b48138524ef",
+    "url": "/static/css/2.1e66eaca.chunk.css"
+  },
+  {
+    "revision": "d47ee64ea35840801eeb",
+    "url": "/static/css/main.c1dcefd6.chunk.css"
+  },
+  {
+    "revision": "07cc99008b48138524ef",
+    "url": "/static/js/2.08de16e5.chunk.js"
+  },
+  {
+    "revision": "d47ee64ea35840801eeb",
+    "url": "/static/js/main.4ae2c52a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "05342f4ca8e9ab746803de8a63aad3cb",
+    "url": "/static/media/mbct-logo.05342f4c.png"
+  },
+  {
+    "revision": "496c3ff7aa626a5cea7cbe50ee15fdc9",
+    "url": "/static/media/wt_400x400.496c3ff7.jpg"
+  }
+]);
