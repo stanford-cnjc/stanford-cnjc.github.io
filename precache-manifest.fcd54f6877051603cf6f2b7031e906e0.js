d02bfdc5cf9a9464c9908b657cf30e7816cde6ec
Eshed Margalit <eshed.margalit@gmail.com>
2020-07-01 20:51:45 -0700

    Updates

---
+++ precache-manifest.fcd54f6877051603cf6f2b7031e906e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "120a87999dc7d3d7f7c7069dda40d60e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "615a49547cee986a2119",
+    "url": "/static/css/2.1e66eaca.chunk.css"
+  },
+  {
+    "revision": "7656cf0a949d7757dbe1",
+    "url": "/static/css/main.1e92e79b.chunk.css"
+  },
+  {
+    "revision": "615a49547cee986a2119",
+    "url": "/static/js/2.08de16e5.chunk.js"
+  },
+  {
+    "revision": "7656cf0a949d7757dbe1",
+    "url": "/static/js/main.34c79a09.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "05342f4ca8e9ab746803de8a63aad3cb",
+    "url": "/static/media/mbct-logo.05342f4c.png"
+  },
+  {
+    "revision": "496c3ff7aa626a5cea7cbe50ee15fdc9",
+    "url": "/static/media/wt_400x400.496c3ff7.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.47d7594da2e21ae170f0a41bfddf3ee4.js"
+  "/precache-manifest.fcd54f6877051603cf6f2b7031e906e0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
